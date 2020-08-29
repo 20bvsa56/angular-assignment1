a02516b2cb7469c31485a868ebe0801b43641696
Bivisha Karki <bivisha2056@gmail.com>
2020-08-29 12:49:09 +0545

    Final code

--- app.js
+++ app.js
@@ -1,31 +1,29 @@
 (function () {
-'use strict';
+    'use strict';
 
-angular.module('LunchChecker', [])
-.controller('LunchCheckerController', LunchCheckerController);
+    angular.module('LunchChecker', [])
+    .controller('LunchCheckerController', LunchCheckerController);
 
-LunchCheckerController.$inject = ['$scope'];
-function LunchCheckerController($scope) {
-  $scope.textbox="".split(',');
-  $scope.displayMessage="Hi there!";
-   
-  $scope.getValue = function (){
-  	var lunchValue = checkNumberOfLunchItem($scope.textbox);
-    $scope.finalValue = lunchValue;
-  };
+    LunchCheckerController.$inject = ['$scope'];
+    
+    function LunchCheckerController($scope) {
+        $scope.item = '';
+        
+        $scope.check = function () {
+            const words = $scope.item.split(' ');
 
-  function checkNumberOfLunchItem (string) {
-  	for (var i = 0; i < string.length; i++) {
-  		if (string[i] <= 3){
-  			return "Enjoy!";
-  		} 
-  		else{
-  			return "Too much!";
-  		}
-  	}
+            console.log(words.length);
+            for (let i = 0; i < words.length; i++) {
+                if (words[i] == '' || words[i] == null) {
+                    return $scope.message = 'Please enter items!'; 
+                } else if (words.length <= 3) {
+                    return $scope.message = 'Enjoy!';
+                } else if (words.length > 3) {
+                    return $scope.message = 'Too Much!';
+                }
+            }
+        }
+    }
 
-  };
-
-}
 })();
 
